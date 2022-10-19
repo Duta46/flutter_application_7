e3e75ccd4d9ae92381d2e0dac0dc77228abf9df1
Duta <84756800+Duta46@users.noreply.github.com>
2022-10-19 17:18:49 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_7/views/home_page.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory document = await getApplicationDocumentsDirectory();
+  Hive.init(document.path);
+  await Hive.openBox<String>("users");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
